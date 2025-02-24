eb0f4a8a65a363647c8c3e08a4dea0034dbbb40d
JEdmario16 <jedmario068@gmail.com>
2025-02-24 04:20:12 -0300

    removido botao de sync

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
   config.hosts << "ec2-15-229-70-89.sa-east-1.compute.amazonaws.com"
+  
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
